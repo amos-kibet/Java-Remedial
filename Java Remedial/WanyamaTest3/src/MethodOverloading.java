740f7aa3762a89124aad442aae8d09a7c10f6564
Amos Kibet <kibetamos97@gmail.com>
2019-07-23 08:53:03 +0300

    Method Overloading Changed
    Method Overloading Changed

--- Java Remedial/WanyamaTest3/src/MethodOverloading.java
+++ Java Remedial/WanyamaTest3/src/MethodOverloading.java
@@ -1,25 +1,26 @@
 public class MethodOverloading {
 
     public static void main(String[] args) {
-        hirepurchase(3000,4,500);
-        compoundinterest(12000.0,12.5,4.0);
-        areaofrectangle(45,31);
-        volumeofacuboid(6,6,7);
+        overloadedMethod(3000,4,500);
+        overloadedMethod(12000.0,12.5,4.0);
+        overloadedMethod(45,31);
+        overloadedMethod(6,6,7);
+        overloadedMethod(3000f,30f,300f);
 
     }
-    public static void hirepurchase(float deposit,float time_in_months,float instalments){
+    public static void overloadedMethod(float deposit,float time_in_months,float instalments){
         float amount=(deposit+instalments*time_in_months);
         System.out.println("The total amount including the hire_purchase is is "+amount);
     }
-    public static void compoundinterest(double p,double r,double t){
+    public static void overloadedMethod(double p,double r,double t){
         double interest=(p*t*0.01*r);
         System.out.println("The compound interest is :"+interest);
     }
-    public static void areaofrectangle(int l,int w){
+    public static void overloadedMethod(int l,int w){
         int area=(l*w);
         System.out.println("The area of the rectangle is :"+area);
     }
-    public static void volumeofacuboid(int p,int q,int z){
+    public static void overloadedMethod(int p,int q,int z){
         int result=(p*q*z);
         System.out.println("The volume is :" +result);
     }
